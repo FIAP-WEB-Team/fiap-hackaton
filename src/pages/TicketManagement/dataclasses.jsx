a05a02f6e68dd71812793c816f9d02a4375bb7f1
Lucas S <lucassoares-2010@hotmail.com>
2023-07-13 15:09:39 -0300

    Implemented ticket views

--- src/pages/TicketManagement/dataclasses.jsx
+++ src/pages/TicketManagement/dataclasses.jsx
@@ -6,19 +6,20 @@ export const SortCriteria = {
 }
 
 export const ProblemType = {
-    DefectiveProduct: 0,
-    ProductNotDelivered: 1,
-    ReceivedWrongProduct: 2
+    DefectiveProduct: "Produto defeituoso",
+    ProductNotDelivered: "Produto não entregue",
+    ReceivedWrongProduct: "Recebeu produto errado"
 }
 
 export const Status = {
-    Pending: 0,
-    Forwarded: 1,
-    Completed: 2
+    Pending: "PENDENTE",
+    Forwarded: "ENCAMINHADO",
+    Completed: "COMPLETADO"
 }
 
 export class Ticket {
-    constructor(problemType, status, initDate, updateDate, client) {
+    constructor(id, problemType, status, initDate, updateDate, client) {
+        this.id = id
         this.problemType = problemType
         this.status = status
         this.initDate = initDate
@@ -38,10 +39,10 @@ export class Filter {
 }
 
 export const DUMMY_TICKETS = [
-    new Ticket(ProblemType.DefectiveProduct, Status.Pending, new Date(2023, 7, 1, 22, 51, 34), new Date(2023, 7, 11, 22, 51, 34), "Derbie Monolith"),
-    new Ticket(ProblemType.ProductNotDelivered, Status.Pending, new Date(2023, 7, 2, 22, 51, 34), new Date(2023, 7, 10, 22, 51, 34), "Claython Undefined"),
-    new Ticket(ProblemType.DefectiveProduct, Status.Completed, new Date(2023, 7, 9, 3, 51, 34), new Date(2023, 7, 9, 22, 51, 34), "Giba Overflow"),
-    new Ticket(ProblemType.ReceivedWrongProduct, Status.Completed, new Date(2023, 7, 5, 22, 51, 34), new Date(2023, 7, 8, 22, 51, 34), "Clovis Null Pointer"),
-    new Ticket(ProblemType.ReceivedWrongProduct, Status.Forwarded, new Date(2023, 7, 4, 21, 51, 34), new Date(2023, 7, 7, 22, 51, 34), "Chokito Aborted"),
-    new Ticket(ProblemType.ProductNotDelivered, Status.Forwarded, new Date(2023, 7, 4, 22, 51, 34), new Date(2023, 7, 6, 22, 51, 34), "Duranduran Linker Error")
+    new Ticket("aoko32", ProblemType.DefectiveProduct, Status.Pending, new Date(2023, 7, 1, 22, 51, 34), new Date(2023, 7, 11, 22, 51, 34), "Derbie Monolith"),
+    new Ticket("12ssdA", ProblemType.ProductNotDelivered, Status.Pending, new Date(2023, 7, 2, 22, 51, 34), new Date(2023, 7, 10, 22, 51, 34), "Claython Undefined"),
+    new Ticket("76fAsq", ProblemType.DefectiveProduct, Status.Completed, new Date(2023, 7, 9, 3, 51, 34), new Date(2023, 7, 9, 22, 51, 34), "Giba Overflow"),
+    new Ticket("0Okd29", ProblemType.ReceivedWrongProduct, Status.Completed, new Date(2023, 7, 5, 22, 51, 34), new Date(2023, 7, 8, 22, 51, 34), "Clovis Null Pointer"),
+    new Ticket("Dop4PQ", ProblemType.ReceivedWrongProduct, Status.Forwarded, new Date(2023, 7, 4, 21, 51, 34), new Date(2023, 7, 7, 22, 51, 34), "Chokito Aborted"),
+    new Ticket("Mnbp25", ProblemType.ProductNotDelivered, Status.Forwarded, new Date(2023, 7, 4, 22, 51, 34), new Date(2023, 7, 6, 22, 51, 34), "Duranduran Linker Error")
 ]
--- src/pages/TicketManagement/index.jsx
+++ src/pages/TicketManagement/index.jsx
@@ -1,26 +1,72 @@
 import React, { useState } from 'react';
 import styles from './TicketManagement.module.scss';
-import {SortCriteria, Filter, Status, Ticket, DUMMY_TICKETS} from './dataclasses.jsx';
+
+import FilterIcon from '../../assets/images/icons/filter-icon.svg';
+import SortAscIcon from '../../assets/images/icons/arrow-down-short-wide.svg';
+import SortDescIcon from '../../assets/images/icons/arrow-up-wide-short.svg';
+import { SortCriteria, Filter, Status, Ticket, DUMMY_TICKETS } from './dataclasses.jsx';
+import TicketCard from '../../components/TicketCard'
+import FilterOverlay from '../../components/FilterOverlay'
 
 
-function filterTickets() {
+function filterTickets(tickets, filters, setDisplayTickets) {
     var filter = new Filter(false, false, false)
-    
+
+    setDisplayTickets(tickets)
+}
+
+function sortTickets(tickets, sortCriteria, setDisplayTickets) {
+    if (sortCriteria === SortCriteria.DateAsc)
+        tickets.sort((ticket1, ticket2) => ticket1.initDate - ticket2.initDate)
+    else if (sortCriteria === SortCriteria.DateDesc)
+        tickets.sort((ticket1, ticket2) => ticket2.initDate - ticket1.initDate)
+    else if (sortCriteria === SortCriteria.LastUpdateAsc)
+        tickets.sort((ticket1, ticket2) => ticket1.updateDate - ticket2.updateDate)
+    else
+        tickets.sort((ticket1, ticket2) => ticket2.updateDate - ticket1.updateDate)
+}
+
+function ticketButtonPressed(ticket) {
+    console.log(ticket)
 }
 
 export default function TicketManagement() {
     const [filters, setFilters] = useState(new Filter(false, false, false))
     const [sortCriteria, setSortCriteria] = useState(SortCriteria.DateDesc)
     const [tickets, setTickets] = useState(DUMMY_TICKETS)
-    const [displayTickets, setDisplayTickets] = useState([])
+    const [displayTickets, setDisplayTickets] = useState(DUMMY_TICKETS)
+    const [isFilterOverlayVisible, setIsFilterOverlayVisible] = useState(true)
+    const [isSortOverlayVisible, setIsSortOverlayVisible] = useState(false)
 
-    console.log(tickets)
+    console.log(displayTickets)
+
+    const ticketsShow = displayTickets.map((ticket) => {
+        return (
+            <div key={ticket.id}>
+                <TicketCard ticket={ticket} clickCallback={ticketButtonPressed} />
+            </div>
+        )
+    })
+
+    var sortIcon = (sortCriteria == SortCriteria.DateAsc || sortCriteria == SortCriteria.LastUpdateAsc) ? SortAscIcon : SortDescIcon;
 
     return (
         <section className={styles.section}>
             <h1>Gerenciamento de reclamações</h1>
             <article className={styles.section__article}>
-                Test
+                <div className={styles.section__article__defaultFilters}>test</div>
+                {isFilterOverlayVisible && <FilterOverlay onClose={() => setIsFilterOverlayVisible(false)} />}
+                <div className={styles.section__article__filterAndSorting}>
+                    <button onClick={() => { setIsFilterOverlayVisible(true) }}>
+                        <img src={FilterIcon} alt="Filter icon" />
+                    </button>
+                    <button onClick={() => { setIsSortOverlayVisible(true) }}>
+                        <img src={sortIcon} alt="Sort icon" />
+                    </button>
+                </div>
+                <div className={styles.section__article__cardContainer}>
+                    {ticketsShow}
+                </div>
             </article>
         </section>
     );
